713abd55f3534614f9b458bd500c38697f0436bb
msnangersme <mail@weiwenng.com>
2016-06-18 18:06:58 +0800

    select time by hour

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <form action="thecage.php" method="GET">
-<input type='time' name='time'>
 <input type='date' name='date'>
+
+<select name='time'>
+  <option value="07:00">7:00 am</option>
+  <option value="08:00">8:00 am</option>
+  <option value="09:00">9:00 am</option>
+  <option value="10:00">10:00 am</option>
+  <option value="11:00">11:00 am</option>
+  <option value="12:00">12:00 pm</option>
+  <option value="13:00">1:00 pm</option>
+  <option value="14:00">2:00 pm</option>
+  <option value="15:00">3:00 pm</option>
+  <option value="16:00">4:00 pm</option>
+  <option value="17:00">5:00 pm</option>
+  <option value="18:00">6:00 pm</option>
+  <option value="19:00">7:00 pm</option>
+  <option value="20:00">8:00 pm</option>
+  <option value="21:00">9:00 pm</option>
+  <option value="22:00">10:00 pm</option>
+  <option value="23:00">11:00 pm</option>
+</select>
+
 <input type="submit" value="Submit">
 </form>
