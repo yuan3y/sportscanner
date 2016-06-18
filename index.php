f971c5929b35e59c019603e64d35c9157aa80994
msnangersme <mail@weiwenng.com>
2016-06-18 17:00:59 +0800

    the cage

---
+++ index.php
@@ -0,0 +1,5 @@
+<form action="thecage.php" method="GET">
+<input type='time' name='time'>
+<input type='date' name='date'>
+<input type="submit" value="Submit">
+</form>
---
+++ thecage.php
@@ -0,0 +1,21 @@
+<?php
+include('simple_html_dom.php');
+
+$date = $_GET['date'];
+$time = $_GET['time'];
+
+$html = file_get_html('http://www.thecage.com.sg/booking_calendar/week_view.php?date='.$date);
+
+// grab all links from site and spot the date
+$links = array();
+foreach($html->find('a') as $a) {
+	$href=$a->href;
+
+ if (strpos($href,$date) && strpos($href,$time)){
+ 	$links[] = $href;
+ 	echo '<a href="'.$href.'"">';
+ 	echo substr($href, strpos($href,"loc=loc")+7,1);
+ 	echo "</a><br/>";
+ }
+}
+?>
