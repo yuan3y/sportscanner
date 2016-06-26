85780a4e9bfb27f8f6f8b488f5eda33140eab440
msnangersme <mail@weiwenng.com>
2016-06-26 23:35:38 +0800

    added basic styling

--- index.php
+++ index.php
@@ -4,11 +4,18 @@
 <meta charset="utf-8">
 <title>Sportscanner</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+<link rel="stylesheet" href="styles.css">
 </head>
+
+<body>
+<center>
+<div class="searchBox">
+<h1>Find futsal pitches</h1>
 <form action="search.php" method="GET">
-<input type='date' name='date' value='<?php echo date("Y-m-d");?>'>
-Start:
-<select name='startTime'>
+<input type='date' name='date' style="width:295px; font-size:20px; height:40px;" value='<?php echo date("Y-m-d");?>'>
+<br><br>
+<select name='startTime' class="searchBoxTime">
+  <option selected disabled>Start</option>
   <option value="07:00">7:00 am</option>
   <option value="08:00">8:00 am</option>
   <option value="09:00">9:00 am</option>
@@ -27,9 +34,8 @@ Start:
   <option value="22:00">10:00 pm</option>
   <option value="23:00">11:00 pm</option>
 </select>
-End:
-<select name='endTime'>
-  <option value="08:00">8:00 am</option>
+<select name='endTime' class="searchBoxTime">
+  <option selected disabled>End</option>
   <option value="09:00">9:00 am</option>
   <option value="10:00">10:00 am</option>
   <option value="11:00">11:00 am</option>
@@ -47,9 +53,13 @@ End:
   <option value="23:00">11:00 pm</option>
   <option value="24:00">12 Midnight</option>  
 </select>
-<input type="submit" value="Search">
+<br><br>
+<input type="submit" value="Search" class="btn">
 </form>
 
 <br>
-</html>
+</div>
+</center>
 
+</body>
+</html>
--- search.php
+++ search.php
@@ -4,6 +4,7 @@
 <meta charset="utf-8">
 <title>Sportscanner</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+<link rel="stylesheet" href="styles.css">
 </head>
 <?php
 include('simple_html_dom.php');
@@ -12,52 +13,57 @@ $startTime = $_GET['startTime'];
 $endTime = $_GET['endTime'];
 $range = $endTime - $startTime;
 ?>
+<body>
+<center>
+<div class="searchBox">
+<h1>Find futsal pitches</h1>
 <form action="search.php" method="GET">
-<input type='date' name='date' value='<?php echo $date;?>'>
-Start:
-<select name='startTime'>
-  <option <?php if($startTime == '07:00'){echo("selected");}?> value="07:00">7:00 am</option>
-  <option <?php if($startTime == '08:00'){echo("selected");}?> value="08:00">8:00 am</option>
-  <option <?php if($startTime == '09:00'){echo("selected");}?> value="09:00">9:00 am</option>
-  <option <?php if($startTime == '10:00'){echo("selected");}?> value="10:00">10:00 am</option>
-  <option <?php if($startTime == '11:00'){echo("selected");}?> value="11:00">11:00 am</option>
-  <option <?php if($startTime == '12:00'){echo("selected");}?> value="12:00">12:00 pm</option>
-  <option <?php if($startTime == '13:00'){echo("selected");}?> value="13:00">1:00 pm</option>
-  <option <?php if($startTime == '14:00'){echo("selected");}?> value="14:00">2:00 pm</option>
-  <option <?php if($startTime == '15:00'){echo("selected");}?> value="15:00">3:00 pm</option>
-  <option <?php if($startTime == '16:00'){echo("selected");}?> value="16:00">4:00 pm</option>
-  <option <?php if($startTime == '17:00'){echo("selected");}?> value="17:00">5:00 pm</option>
-  <option <?php if($startTime == '18:00'){echo("selected");}?> value="18:00">6:00 pm</option>
-  <option <?php if($startTime == '19:00'){echo("selected");}?> value="19:00">7:00 pm</option>
-  <option <?php if($startTime == '20:00'){echo("selected");}?> value="20:00">8:00 pm</option>
-  <option <?php if($startTime == '21:00'){echo("selected");}?> value="21:00">9:00 pm</option>
-  <option <?php if($startTime == '22:00'){echo("selected");}?> value="22:00">10:00 pm</option>
-  <option <?php if($startTime == '23:00'){echo("selected");}?> value="23:00">11:00 pm</option>
+<input type='date' name='date' style="width:295px; font-size:20px; height:40px;" value='<?php echo date("Y-m-d");?>'>
+<br><br>
+<select name='startTime' class="searchBoxTime">
+  <option selected disabled>Start</option>
+  <option value="07:00">7:00 am</option>
+  <option value="08:00">8:00 am</option>
+  <option value="09:00">9:00 am</option>
+  <option value="10:00">10:00 am</option>
+  <option value="11:00">11:00 am</option>
+  <option value="12:00">12:00 pm</option>
+  <option value="13:00">1:00 pm</option>
+  <option value="14:00">2:00 pm</option>
+  <option value="15:00">3:00 pm</option>
+  <option value="16:00">4:00 pm</option>
+  <option value="17:00">5:00 pm</option>
+  <option value="18:00">6:00 pm</option>
+  <option value="19:00">7:00 pm</option>
+  <option value="20:00">8:00 pm</option>
+  <option value="21:00">9:00 pm</option>
+  <option value="22:00">10:00 pm</option>
+  <option value="23:00">11:00 pm</option>
 </select>
-End:
-<select name='endTime'>
-  <option <?php if($endTime == '08:00'){echo("selected");}?> value="08:00">8:00 am</option>
-  <option <?php if($endTime == '09:00'){echo("selected");}?> value="09:00">9:00 am</option>
-  <option <?php if($endTime == '10:00'){echo("selected");}?> value="10:00">10:00 am</option>
-  <option <?php if($endTime == '11:00'){echo("selected");}?> value="11:00">11:00 am</option>
-  <option <?php if($endTime == '12:00'){echo("selected");}?> value="12:00">12:00 pm</option>
-  <option <?php if($endTime == '13:00'){echo("selected");}?> value="13:00">1:00 pm</option>
-  <option <?php if($endTime == '14:00'){echo("selected");}?> value="14:00">2:00 pm</option>
-  <option <?php if($endTime == '15:00'){echo("selected");}?> value="15:00">3:00 pm</option>
-  <option <?php if($endTime == '16:00'){echo("selected");}?> value="16:00">4:00 pm</option>
-  <option <?php if($endTime == '17:00'){echo("selected");}?> value="17:00">5:00 pm</option>
-  <option <?php if($endTime == '18:00'){echo("selected");}?> value="18:00">6:00 pm</option>
-  <option <?php if($endTime == '19:00'){echo("selected");}?> value="19:00">7:00 pm</option>
-  <option <?php if($endTime == '20:00'){echo("selected");}?> value="20:00">8:00 pm</option>
-  <option <?php if($endTime == '21:00'){echo("selected");}?> value="21:00">9:00 pm</option>
-  <option <?php if($endTime == '22:00'){echo("selected");}?> value="22:00">10:00 pm</option>
-  <option <?php if($endTime == '23:00'){echo("selected");}?> value="23:00">11:00 pm</option> 
-  <option <?php if($endTime == '24:00'){echo("selected");}?> value="24:00">12 Midnight</option>
+<select name='endTime' class="searchBoxTime">
+  <option selected disabled>End</option>
+  <option value="09:00">9:00 am</option>
+  <option value="10:00">10:00 am</option>
+  <option value="11:00">11:00 am</option>
+  <option value="12:00">12:00 pm</option>
+  <option value="13:00">1:00 pm</option>
+  <option value="14:00">2:00 pm</option>
+  <option value="15:00">3:00 pm</option>
+  <option value="16:00">4:00 pm</option>
+  <option value="17:00">5:00 pm</option>
+  <option value="18:00">6:00 pm</option>
+  <option value="19:00">7:00 pm</option>
+  <option value="20:00">8:00 pm</option>
+  <option value="21:00">9:00 pm</option>
+  <option value="22:00">10:00 pm</option>
+  <option value="23:00">11:00 pm</option>
+  <option value="24:00">12 Midnight</option>  
 </select>
-<input type="submit" value="Search">
+<br><br>
+<input type="submit" value="Search" class="btn">
 </form>
-
 <br>
+</div>
 <div id="results"><?php
 $i=0;
 $rangeArray = array();
@@ -85,5 +91,7 @@ if(theDiv.innerHTML.length == 0){
     theDiv.style.display="inline";
 }
 </script>
-</html>
+</center>
 
+</body>
+</html>
